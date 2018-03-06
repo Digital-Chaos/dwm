94c8e09f7942058ed3c4fd0cd3883b09c65ee98c
Digital-Chaos <james.wright@jigsawdezign.com>
2018-03-06 14:34:17 +0000

    Reduce window snap distance

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 1;        /* window border (pixels) */
+static const unsigned int snap      = 8;        /* snap distance (pixels) */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=7" };
