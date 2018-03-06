8ca8657f99004e5dd76d0ddf903d47866874380b
Digital-Chaos <james.wright@jigsawdezign.com>
2018-03-06 09:38:06 +0000

    Customise tags and rules

--- config.h
+++ config.h
@@ -19,16 +19,20 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "term", "mail", "web", "code", "game", "net", "music", "gfx", "misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class          	instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",         	NULL,       NULL,       0,            1,           -1 },
+	{ "Thunderbird",  	NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Firefox",      	NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Sublime_text", 	NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "jetbrains-idea-ce", 	NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
