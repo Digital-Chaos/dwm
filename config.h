9e0c2ae425dfe1b97f0ac5ff057235889e6cd9a1
Digital-Chaos <james.wright@jigsawdezign.com>
2018-03-14 19:11:54 +0000

    Reduced tags to just five, added rule for Sylpheed

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "term", "mail", "web", "code", "game", "net", "music", "gfx", "misc" };
+static const char *tags[] = { "term", "mail", "web", "code", "misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class          	instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",         	NULL,       NULL,       0,            1,           -1 },
+	{ "Sylpheed",  		NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Thunderbird",  	NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Firefox",      	NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Sublime_text", 	NULL,       NULL,       1 << 3,       0,           -1 },
