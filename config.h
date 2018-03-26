e98b003ad36417ce3ea20e397faa5d6ad7fe5bc0
Digital-Chaos <james.wright@jigsawdezign.com>
2018-03-26 02:51:53 +0100

    Add lock command and hotkey

--- config.h
+++ config.h
@@ -74,6 +74,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]        = { "urxvt", NULL };
+static const char *lockcmd[]        = { "slock", NULL };
 static const char *brightupcmd[]    = { "intel_backlight", "incr", NULL };
 static const char *brightdowncmd[]  = { "intel_backlight", "decr", NULL };
 static const char *volumemutecmd[]  = { "mixer", "vol",   "0", NULL };
@@ -88,6 +89,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_F11,    spawn,          {.v = volumedowncmd } },
 	{ MODKEY,                       XK_F12,    spawn,          {.v = volumeupcmd } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
