91797d93a02fb82847f24e06bcbdd4a5e47d72c5
James Wright <james.wright@jigsawdezign.com>
2019-03-10 14:34:22 +0000

    Update config.h to change default terminal from urxvt to xterm

--- config.h
+++ config.h
@@ -77,7 +77,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]        = { "urxvt", NULL };
+static const char *termcmd[]        = { "xterm", NULL };
 static const char *lockcmd[]        = { "slock", NULL };
 static const char *brightupcmd[]    = { "intel_backlight", "incr", NULL };
 static const char *brightdowncmd[]  = { "intel_backlight", "decr", NULL };
