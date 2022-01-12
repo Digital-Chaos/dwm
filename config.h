0e7bac20214b0ee694b9539f22f1868ca7f55e59
James Wright <james.wright@digital-chaos.com>
2022-01-12 11:30:39 +0000

    Add missing lockfullscreen flag to config.h

--- config.h
+++ config.h
@@ -62,6 +62,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 /* layouts - first entry is default*/
 static const Layout layouts[] = {
