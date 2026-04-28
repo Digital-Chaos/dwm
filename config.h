b4295a19064f2c971b4092755afd07707efc73d1
James Wright <james.wright@digital-chaos.com>
2026-04-28 22:36:54 +0100

    Add missing refreshrate constant to config.h

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
 /* layouts - first entry is default*/
 static const Layout layouts[] = {
